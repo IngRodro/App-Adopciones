1b206a4bc0e933f891841f49a3cb890199359332
ElRodroUwU15 <rr0213032019@unab.edu.sv>
2021-11-14 20:30:50 -0600

    Va quedando mamalon

--- app/src/main/java/com/example/appmascota/MainActivity.kt
+++ app/src/main/java/com/example/appmascota/MainActivity.kt
@@ -3,22 +3,35 @@ package com.example.appmascota
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var etUser: EditText
     private lateinit var etPass: EditText
     private lateinit var btnRegister: Button
+    private lateinit var btnLogin: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnRegister = findViewById(R.id.btnRegister)
+        btnLogin = findViewById(R.id.btnLogin)
         btnRegister.setOnClickListener{
             val siguienteActivity = Intent(this,RegisterActivity::class.java)
             startActivity(siguienteActivity)
         }
+        etUser = findViewById(R.id.etUser)
+        etPass= findViewById(R.id.etPass)
+
+        btnLogin.setOnClickListener{
+            if(etUser.getText().toString().equals("admin") && etPass.getText().toString().equals("admin")){
+                //correct
+                Toast.makeText(this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+            }else
+            //incorrect
+                Toast.makeText(this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
--- app/src/main/java/com/example/appmascota/RegisterActivity.kt
+++ app/src/main/java/com/example/appmascota/RegisterActivity.kt
@@ -2,10 +2,123 @@ package com.example.appmascota
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import android.widget.ArrayAdapter
+
+
+
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var etNames: EditText
+    private lateinit var etLastNames: EditText
+    private lateinit var spDepartamento: Spinner
+    private lateinit var spMunicipio: Spinner
+    private lateinit var etUser: EditText
+    private lateinit var etPass: EditText
+    private lateinit var etPassConfirmed: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        etNames = findViewById(R.id.etNames)
+        etLastNames = findViewById(R.id.etLastNames)
+        spDepartamento = findViewById(R.id.spDepartamento)
+        spMunicipio = findViewById(R.id.spMunicipio)
+        etUser = findViewById(R.id.etUser)
+        etPass = findViewById(R.id.etPass)
+        etPassConfirmed = findViewById(R.id.etPassConfirmed)
+
+
+        val departamentos = arrayOf(
+            "Seleccione una opcion",
+            "San Salvador",
+            "Chalatenango",
+            "Sonsonate",
+            "La Paz",
+            "La Libertad",
+            "San Miguel",
+            "Usulután",
+            "Cabañas",
+            "Santa Ana",
+            "San Vicente",
+            "La Unión",
+            "Morazán",
+            "Cuscatlán",
+            "Ahuachapán"
+        )
+
+        val municipiosvacio = arrayOf(
+            "Seleccione una opcion"
+        )
+        val adaptador = ArrayAdapter(this, R.layout.item_spin, R.id.textview, departamentos)
+        spDepartamento.adapter = adaptador
+
+        //Funcion para recoger un listado de municipios de acuerdo a un departamento
+        fun getMunicipios(departamento:String):ArrayList<String>{
+            val municipios= arrayListOf<String>()
+            when(departamento){
+                "Seleccione una opcion"->municipios.addAll(listOf("Seleccione una opcion"))
+                "Chalatenango"->municipios.addAll(listOf("Seleccione una opcion","Agua Caliente","Arcatao","Azacualpa","Chalatenango","Citalá","Comalapa","Concepción Quezaltepeque",
+                    "Dulce Nombre de María","El Carrizal","El Paraíso","La Laguna","La Palma","La Reina","Las Vueltas","Nombre de Jesús","Nueva Concepción",
+                    "Nueva Trinidad","Ojos de Agua","Potonico","San Antonio de la Cruz","San Antonio Los Ranchos","San Fernando","San Francisco Lempa",
+                    "San Francisco Morazán","San Ignacio","San Isidro Labrador","San José Cancasque (Cancasque)","San José Las Flores","San Luis del Carmen",
+                    "San Miguel de Mercedes","San Rafael","Santa Rita","Tejutla"))
+                "San Salvador"->municipios.addAll(listOf("Seleccione una opcion","Aguilares","Apopa","Ayutuxtepeque","Ciuddad Delgado","Cuscatancingo","El Paisnal","Guazapa","Ilopango",
+                    "Mejicanos","Nejapa","Panchimalco","Rosario de Mora","San Marcos","San Martín","San Salvador","Santiago Texacuangos","Santo Tomás",
+                    "Soyapango","Tonacatepeque"))
+                "Sonsonate"->municipios.addAll(listOf("Seleccione una opcion","Acajutla","Armenia","Caluco","Cuisnahuat","Izalco","Juayúa","Nahuizalco","Nahulingo","Salcoatitán",
+                    "San Antonio del Monte","San Julián","Santa Catarina Masahuat","Santa Isabel Ishuatán","Santo Domingo de Guzmán","Sonsonate",
+                    "Sonzacate"))
+                "La Paz"->municipios.addAll(listOf("Seleccione una opcion","Cuyultitán","El Rosario (Rosario de La Paz)","Jerusalén","Mercedes La Ceiba","Olocuilta","Paraíso de Osorio",
+                    "San Antonio Masahuat","San Emigdio","San Francisco Chinameca","San Juan Nonualco","San Juan Talpa","San Juan Tepezontes",
+                    "San Luis La Herradura","San Luis Talpa","San Miguel Tepezontes","San Pedro Masahuat","San Pedro Nonualco","San Rafael Obrajuelo",
+                    "Santa María Ostuma","Santiago Nonualco","Tapalhuaca","Zacatecoluca"))
+                "La Libertad"->municipios.addAll(listOf("Seleccione una opcion","Antiguo Cuscatlán","Chiltiupán","Ciudad Arce","Colón","Comasagua","Huizúcar","Jayaque","Jicalapa",
+                    "La Libertad","Santa Tecla (Nueva San Salvador)","Nuevo Cuscatlán","San Juan Opico","Quezaltepeque","Sacacoyo",
+                    "San José Villanueva","San Matías","San Pablo Tacachico","Talnique","Tamanique","Teotepeque","Tepecoyo","Zaragoza"))
+                "San Miguel"->municipios.addAll(listOf("Seleccione una opcion","Carolina","Chapeltique","Chinameca","Chirilagua","Ciudad Barrios","Comacarán","El Tránsito","Lolotique",
+                    "Moncagua","Nueva Guadalupe","Nuevo Edén de San Juan","Quelepa","San Antonio del Mosco","San Gerardo","San Jorge",
+                    "San Luis de la Reina","San Miguel","San Rafael Oriente","Sesori","Uluazapa"))
+                "Usulután"->municipios.addAll(listOf("Seleccione una opcion","Alegría","Berlín","California","Concepción Batres","El Triunfo","Ereguayquín","Estanzuelas","Jiquilisco",
+                    "Jucuapa","Jucuarán","Mercedes Umaña","Nueva Granada","Ozatlán","Puerto El Triunfo","San Agustín","San Buenaventura",
+                    "San Dionisio","San Francisco Javier","Santa Elena","Santa María","Santiago de María","Tecapán","Usulután"))
+                "Cabañas"->municipios.addAll(listOf("Seleccione una opcion","Cinquera","Dolores (Villa Doleres)","Guacotecti","Ilobasco","Jutiapa","San Isidro",
+                    "Sensuntepeque","Tejutepeque","Victoria"))
+                "Santa Ana"->municipios.addAll(listOf("Seleccione una opcion","Candelaria de la Frontera","Chalchuapa","Coatepeque","El Congo","El Porvenir","Masahuat","Metapán",
+                    "San Antonio Pajonal","San Sebastián Salitrillo","Santa Ana","Santa Rosa Guachipilín","Santiago de la Frontera","Texistepeque"))
+                "San Vicente"->municipios.addAll(listOf("Seleccione una opcion","Apastepeque","Guadalupe","San Cayetano Istepeque","San Esteban Catarina","San Ildefonso","San Lorenzo",
+                    "San Sebastián","San Vicente","Santa Clara","Santo Domingo","Tecoluca","Tepetitán","Verapaz"))
+                "La Unión"->municipios.addAll(listOf("Seleccione una opcion","Anamorós","Bolívar","Concepción de Oriente","Conchagua","El Carmen","El Sauce","Intipucá","La Unión","Lilisque",
+                    "Meanguera del Golfo","Nueva Esparta","Pasaquina","Polorós","San Alejo","San José","Santa Rosa de Lima","Yayantique","Yucuaiquín"))
+                "Morazán"->municipios.addAll(listOf("Seleccione una opcion","Arambala","Cacaopera","Chilanga","Corinto","Delicias de Concepción","El Divisadero","El Rosario","Gualococti",
+                    "Guatajiagua","Joateca","Jocoaitique","Jocoro","Lolotiquillo","Meanguera","Osicala","Perquín","San Carlos","San Fernando",
+                    "San Francisco Gotera","San Isidro","San Simón","Sensembra","Sociedad","Torola","Yamabal","Yoloaiquín"))
+                "Cuscatlán"->municipios.addAll(listOf("Seleccione una opcion","Candelaria","Cojutepeque","El Carmen","El Rosario","Monte San Juan","Oratorio de Concepción","San Bartolomé Perulapía",
+                    "San Cristóbal","San José Guayabal","San Pedro Perulapán","San Rafael Cedros","San Ramón","Santa Cruz Analquito","Santa Cruz Michapa",
+                    "Suchitoto","Tenancingo"))
+                "Ahuachapán"->municipios.addAll(listOf("Seleccione una opcion","Ahuachapán","Apaneca","Atiquizaya","Concepción de Ataco","El Refugio","Guaymango","Jujutla",
+                    "San Francisco Menéndez","San Lorenzo","San Pedro Puxtla","Tacuba","Turín"))
+            }
+            return municipios
+        }
+
+
+        //Esta parte simula el evento ItemChange de una lista
+        spDepartamento?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //Codigo para cargar el otro spinner
+                var depart:String=spDepartamento.selectedItem.toString()
+                var Municipios = getMunicipios(depart)
+                val adaptadorMuni = ArrayAdapter(applicationContext,  R.layout.item_spin, R.id.textview, Municipios)
+               spMunicipio.adapter=adaptadorMuni
+            }
+        }
     }
+
+    fun Hola(){}
 }
