00facbc328984b0685ac80709d2b124e1f43746d
RaquelCastr <73264587+RaquelCastr@users.noreply.github.com>
2021-11-19 19:35:21 -0600

    RegistrarMascota

---
+++ AppMascota/app/src/main/java/com/example/appmascota/Modelos/Pets.kt
@@ -0,0 +1,7 @@
+package com.example.appmascota.Modelos
+
+import java.io.File
+
+data class Pets(var nombre:String, var sexo:String, var edad: Int, var foto: ByteArray?) {
+
+}
---
+++ AppMascota/app/src/main/java/com/example/appmascota/Modelos/UserResponse.kt
@@ -0,0 +1,5 @@
+package com.example.appmascota.Modelos
+
+data class UserResponse (var username:String, var password: String, var estado:String) {
+
+}
