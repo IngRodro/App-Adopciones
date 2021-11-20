9b3dad303d1bbe881e2e5728fb48782823669afa
Rodrigo Ramirez <62961147+ElRodroUwU15@users.noreply.github.com>
2021-11-19 20:25:01 -0600

    Arreglando Problemas Tecnicos

--- AppMascota/app/src/main/java/com/example/appmascota/Modelos/UserResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.appmascota.Modelos
-
-data class UserResponse (var username:String, var password: String, var estado:String) {
-
-}
--- AppMascota/app/src/main/java/com/example/appmascota/Modelos/Users.kt
+++
@@ -1,5 +0,0 @@
-package com.example.appmascota.Modelos
-
-data class Users(var iduser:Int?, var username:String, var password: String,var names:String, var lastnames:String, var departamento:String, var municipio:String) {
-
-}
