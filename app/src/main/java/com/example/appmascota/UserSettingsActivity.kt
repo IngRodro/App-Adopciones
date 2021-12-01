eefbd78f8558f469a935ca0df0f12749b66328ac
LuiZDiaAz <62961646+LuiZDiaAz@users.noreply.github.com>
2021-11-30 21:09:59 -0600

    Diseño UserSettings

---
+++ app/src/main/java/com/example/appmascota/UserSettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appmascota
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_settings)
+    }
+}
